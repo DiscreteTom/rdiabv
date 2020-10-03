a9ed9af30a7c9716316b86fc1e5ba1ef0467fcbe
DiscreteTom <discrete_tom@outlook.com>
2020-10-03 21:20:33 +0800

    update examples/hello-world, add htrm support

---
+++ examples/hello-world/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/DiscreteTom/rdiabv"
+)
+
+// Define block count
+const n = 10000
+const timesForHTRM = 10
+
+func main() {
+	runDHDD()
+	runHTRM()
+}
+
+func runDHDD() {
+	// Init DHDD
+	dhdd := rdiabv.NewDHDD(n, time.Now().UnixNano()).
+		InitBuffers(rdiabv.NewDefaultBlock()) // Use default block to init buffers
+
+	// Merge blocks
+	for i := 0; i < n; i++ {
+		dhdd.MergeBlock(i, rdiabv.DefaultBlockGenerator())
+	}
+
+	// Check
+	fmt.Println(dhdd.CheckAllBuffers()) // => true
+}
+
+func runHTRM() {
+	// Init HTRM
+	htrm := rdiabv.NewHTRM(timesForHTRM).
+		InitBuffers(rdiabv.NewDefaultBlock()) // Use default block to init buffers
+
+	// Merge blocks
+	for i := 0; i < n; i++ {
+		htrm.MergeBlock(i, rdiabv.DefaultBlockGenerator())
+	}
+
+	// Check
+	fmt.Println(htrm.CheckAllBuffers()) // => true
+}
