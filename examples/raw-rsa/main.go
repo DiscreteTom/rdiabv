4869006058799e918e7cea4324017e7491da8294
DiscreteTom <discrete_tom@outlook.com>
2020-10-03 21:24:59 +0800

    update examples/raw-rsa, add htrm support

--- examples/raw-rsa/main.go
+++ examples/raw-rsa/main.go
@@ -14,6 +14,7 @@ import (
 const rsaKeyBits = 4096
 const chunkSize = 256 // Byte count of a block's data field.
 const blockCount = 1024
+const timesForHTRM = 10
 const dataFilename = "data.bin"
 const tagFilename = "tag.txt"
 
@@ -28,6 +29,11 @@ func main() {
 	start := time.Now()
 	fmt.Println(runDHDD())
 	fmt.Printf("%v: %v\n", "DHDD", time.Since(start))
+
+	fmt.Println("Running HTRM:")
+	start = time.Now()
+	fmt.Println(runHTRM())
+	fmt.Printf("%v: %v\n", "HTRM", time.Since(start))
 
 	fmt.Println("Running one by one:")
 	start = time.Now()
@@ -73,6 +79,46 @@ func runDHDD() bool {
 
 	fmt.Println("Checking...")
 	return dhdd.CheckAllBuffers()
+}
+
+func runHTRM() bool {
+	// open files
+	dataFile, err := os.Open(dataFilename)
+	if err != nil {
+		panic(err)
+	}
+	tagFile, err := os.Open(tagFilename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(tagFile)
+
+	// init read buffer for data file
+	dataBuffer := make([]byte, chunkSize)
+
+	// init dhdd
+	fmt.Println("Initializing HTRM...")
+	htrm := rdiabv.NewHTRM(timesForHTRM).
+		InitBuffers(NewRawRsaBlock())
+
+	// read & merge
+	fmt.Println("Merging...")
+	for i := 0; i < blockCount; i++ {
+		// for data file, read a chunk
+		_, err := dataFile.Read(dataBuffer)
+		if err != nil {
+			panic(err)
+		}
+		// create block
+		var block = RawRsaBlock{Data: new(big.Int).SetBytes(dataBuffer)}
+		// for tag file, read a line
+		scanner.Scan()
+		block.Tag, _ = new(big.Int).SetString(scanner.Text(), 10)
+		htrm.MergeBlock(i, &block)
+	}
+
+	fmt.Println("Checking...")
+	return htrm.CheckAllBuffers()
 }
 
 // check every block
