15f5cff2cbf9fc34c1b8c77a1164645116f48d17
DiscreteTom <discrete_tom@outlook.com>
2020-10-06 00:43:15 +0800

    update examples/raw-rsa, add parallel api support

--- examples/raw-rsa/run_dhdd.go
+++ examples/raw-rsa/run_dhdd.go
@@ -27,3 +27,24 @@ func runDHDD(fm *FileManager) bool {
 	fmt.Println("Checking...")
 	return dhdd.CheckAllBuffers()
 }
+
+func parallelRunDHDD(fm *FileManager) bool {
+	fm.StartSession()
+	defer fm.EndSession()
+
+	// init dhdd
+	fmt.Println("Parallel initializing DHDD...")
+	dhdd := rdiabv.NewDHDD(fm.blockCount, time.Now().UnixNano()).
+		ParallelInitBuffers(NewRawRsaBlock(fm.rr))
+
+	// read & merge
+	fmt.Println("Parallel merging...")
+	for i := 0; i < fm.blockCount; i++ {
+		// create block
+		var block = RawRsaBlock{Data: fm.NextBlockData(), Tag: fm.NextBlockTag(), Key: fm.rr}
+		dhdd.ParallelMergeBlock(i, &block)
+	}
+
+	fmt.Println("Parallel checking...")
+	return dhdd.ParallelCheckAllBuffers()
+}
--- examples/raw-rsa/run_htrm.go
+++ examples/raw-rsa/run_htrm.go
@@ -28,3 +28,24 @@ func runHTRM(fm *FileManager) bool {
 	fmt.Println("Checking...")
 	return htrm.CheckAllBuffers()
 }
+
+func parallelRunHTRM(fm *FileManager) bool {
+	fm.StartSession()
+	defer fm.EndSession()
+
+	// init HTRM
+	fmt.Println("Parallel initializing HTRM...")
+	htrm := rdiabv.NewHTRM(timesForHTRM).
+		ParallelInitBuffers(NewRawRsaBlock(fm.rr))
+
+	// read & merge
+	fmt.Println("Parallel merging...")
+	for i := 0; i < fm.blockCount; i++ {
+		// create block
+		var block = RawRsaBlock{Data: fm.NextBlockData(), Tag: fm.NextBlockTag(), Key: fm.rr}
+		htrm.ParallelMergeBlock(&block)
+	}
+
+	fmt.Println("Parallel checking...")
+	return htrm.ParallelCheckAllBuffers()
+}
